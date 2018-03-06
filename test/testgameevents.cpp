14cdb9ffb978c0a795ae328550dd82c1965064af
Michał Garapich <michal@garapich.pl>
2018-03-06 02:55:45 +0100

    Add test

--- test/testgameevents.cpp
+++ test/testgameevents.cpp
@@ -1,5 +1,6 @@
 #include <QtTest/QtTest>
 #include "loglistener.h"
+#include "gameevents/playerconnected.h"
 #include "gameevents/statushostname.h"
 #include "gameevents/statusmap.h"
 #include "gameevents/statusplayernumbers.h"
@@ -7,75 +8,71 @@
 class TestGameEvents : public QObject {
     Q_OBJECT
 
-    morgoth::LogListener* logListener;
-
 private slots:
-    void init();
-    void cleanup();
-
-    void statusHostname();
-    void playerNumbers();
-    void map();
+    void status();
+    void playerConnected();
 
 };
 
 
-
-void TestGameEvents::init()
+void TestGameEvents::status()
 {
     QString fileName = QFINDTESTDATA("cmd_status.log");
     QVERIFY(QFile(fileName).exists());
 
-    logListener = new morgoth::LogListener(fileName);
-}
+    morgoth::LogListener* logListener = new morgoth::LogListener(fileName);
+    QVERIFY(logListener);
 
-void TestGameEvents::cleanup()
-{
+    morgoth::StatusHostname* hostnameLine = new morgoth::StatusHostname;
+    QSignalSpy spyHostname(hostnameLine, &morgoth::EventHandler::activated);
+    logListener->installEventHandler(hostnameLine);
+
+    morgoth::StatusPlayerNumbers* playerLine = new morgoth::StatusPlayerNumbers;
+    QSignalSpy spyPlayers(playerLine, &morgoth::EventHandler::activated);
+    logListener->installEventHandler(playerLine);
+
+    morgoth::StatusMap* mapLine = new morgoth::StatusMap;
+    QSignalSpy spyMap(mapLine, &morgoth::EventHandler::activated);
+    logListener->installEventHandler(mapLine);
+
+    logListener->start();
+
+    spyHostname.wait(1000);
+    QCOMPARE(spyHostname.count(), 1);
+    QCOMPARE(hostnameLine->hostname(), "melkor.tf #1");
+
+    spyPlayers.wait(100);
+    QCOMPARE(spyPlayers.count(), 1);
+    QCOMPARE(playerLine->playerCount(), 0);
+    QCOMPARE(playerLine->maxPlayers(), 25);
+
+    spyMap.wait(10);
+    QCOMPARE(spyMap.count(), 1);
+    QCOMPARE(mapLine->map(), "cp_process_final");
+
     logListener->requestInterruption();
     logListener->wait();
 }
 
-void TestGameEvents::statusHostname()
+void TestGameEvents::playerConnected()
 {
+    QString fileName = QFINDTESTDATA("dm-console.log.20180306023605");
+    QVERIFY(QFile(fileName).exists());
+
+    morgoth::LogListener* logListener = new morgoth::LogListener(fileName);
     QVERIFY(logListener);
 
-    morgoth::StatusHostname* hostnameLine = new morgoth::StatusHostname;
-    QSignalSpy spy(hostnameLine, &morgoth::EventHandler::activated);
-    logListener->installEventHandler(hostnameLine);
+    morgoth::PlayerConnected* playerConnected = new morgoth::PlayerConnected;
+    QSignalSpy spy(playerConnected, &morgoth::EventHandler::activated);
+    logListener->installEventHandler(playerConnected);
 
     logListener->start();
-    spy.wait();
+
+    QVERIFY(spy.wait());
     QCOMPARE(spy.count(), 1);
-    QCOMPARE(hostnameLine->hostname(), "melkor.tf #1");
-}
 
-void TestGameEvents::playerNumbers()
-{
-    QVERIFY(logListener);
-
-    morgoth::StatusPlayerNumbers* playerLine = new morgoth::StatusPlayerNumbers;
-    QSignalSpy spy(playerLine, &morgoth::EventHandler::activated);
-    logListener->installEventHandler(playerLine);
-
-    logListener->start();
-    spy.wait();
-    QCOMPARE(spy.count(), 1);
-    QCOMPARE(playerLine->playerCount(), 0);
-    QCOMPARE(playerLine->maxPlayers(), 25);
-}
-
-void TestGameEvents::map()
-{
-    QVERIFY(logListener);
-
-    morgoth::StatusMap* mapLine = new morgoth::StatusMap;
-    QSignalSpy spy(mapLine, &morgoth::EventHandler::activated);
-    logListener->installEventHandler(mapLine);
-
-    logListener->start();
-    spy.wait();
-    QCOMPARE(spy.count(), 1);
-    QCOMPARE(mapLine->map(), "cp_process_final");
+    logListener->requestInterruption();
+    logListener->wait();
 }
 
 QTEST_MAIN(TestGameEvents)
