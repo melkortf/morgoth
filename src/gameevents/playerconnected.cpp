7bbb0000a8a5f8f95f8110b5e8cde4300d5ce4fe
Michał Garapich <michal@garapich.pl>
2018-03-06 02:55:28 +0100

    Add PlayerConnected gameevent

---
+++ src/gameevents/playerconnected.cpp
@@ -0,0 +1,36 @@
+// This file is part of morgoth.
+
+// morgoth is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "playerconnected.h"
+
+namespace morgoth {
+
+PlayerConnected::PlayerConnected(QObject* parent) :
+    EventHandler(Name, parent)
+{
+    
+}
+
+QRegularExpression PlayerConnected::regex() const
+{
+    // https://regex101.com/r/uyPW8m/2
+    return QRegularExpression(
+                "^\\w\\s(\\d{2}\\/\\d{2}\\/\\d{4})\\s-\\s(\\d{2}"
+                ":\\d{2}:\\d{2}):\\s\\\"(.[^\\\"]+)\"\\sconnected,"
+                "\\saddress\\s\\\"(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"
+                "\\.\\d{1,3}:\\d{1,5})\"$");
+}
+
+} // namespace morgoth
