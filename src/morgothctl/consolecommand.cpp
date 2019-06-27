ddf9066f794457e527c68eb5263e82432a147c00
Michał Garapich <michal@garapich.pl>
2019-06-27 13:51:25 +0200

    attach to the given session

--- src/morgothctl/consolecommand.cpp
+++ src/morgothctl/consolecommand.cpp
@@ -20,6 +20,32 @@
 #include "servermanagerinterface.h"
 #include "serverinterface.h"
 #include <QtCore>
+#include <unistd.h>
+
+namespace {
+// FIXME Use TmuxSessionWrapper here
+QString tmuxExec() {
+    constexpr auto TmuxExec = "tmux";
+    static QString tmuxBin;
+
+    if (tmuxBin.isEmpty()) {
+        QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+        QStringList path = env.value("PATH").split(QDir::listSeparator());
+        for (const QString& p: qAsConst(path)) {
+            QString maybe = QDir::cleanPath(p + QDir::separator() + TmuxExec);
+            if (QFile::exists(maybe)) {
+                tmuxBin = maybe;
+                break;
+            }
+        }
+
+        if (tmuxBin.isEmpty())
+            qFatal("Could not find tmux executable; exiting.");
+    }
+
+    return tmuxBin;
+}
+}
 
 int ConsoleCommand::execute(QDBusConnection dbus, const QStringList& arguments, QTextStream& out)
 {
@@ -30,14 +56,14 @@ int ConsoleCommand::execute(QDBusConnection dbus, const QStringList& arguments, 
     parser.addPositionalArgument("name", "The server name.", QStringLiteral("%1 <name>...").arg(command()));
     parser.process(arguments);
 
-    QStringList args = parser.positionalArguments();
-    if (args.isEmpty())
+    QStringList commandArgs = parser.positionalArguments();
+    if (commandArgs.isEmpty())
         parser.showHelp(1);
 
     org::morgoth::ServerManager serverManager(morgoth::MorgothDaemon::dbusServiceName(), "/servers", dbus, qApp);
     Q_ASSERT(serverManager.isValid());
 
-    QString serverName = args.first();
+    QString serverName = commandArgs.first();
     if (!serverManager.servers().contains(serverName)) {
         out << "Server \"" << serverName << "\" does not exist." << endl;
         return 2;
@@ -54,5 +80,24 @@ int ConsoleCommand::execute(QDBusConnection dbus, const QStringList& arguments, 
         return 3;
     }
 
-    return 0;
+    QString tmux = tmuxExec();
+
+    QString session = coordinator.sessionName();
+    QVector<char*> args = {
+        strdup(tmux.toLocal8Bit().constData()),
+        "attach-session",
+        "-t",
+        strdup(session.toLocal8Bit().constData()),
+        NULL
+    };
+
+    QVector<char*> envp;
+    auto se = QProcess::systemEnvironment();
+    std::transform(se.begin(), se.end(), std::back_inserter(envp),
+                   [](const QString& e) -> char* {
+        return strdup(e.toLocal8Bit().constData());
+    });
+    envp.push_back(NULL);
+
+    return execve(tmux.toLocal8Bit().constData(), args.data(), envp.data());
 }
