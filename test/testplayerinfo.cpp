27b717059680cf921361617464ad150ba33f82a9
Michał <michal@garapich.pl>
2019-10-29 17:47:31 +0100

    add TestPlayerInfo::dbusMarshall()

--- test/testplayerinfo.cpp
+++ test/testplayerinfo.cpp
@@ -12,6 +12,7 @@ private:
     void setName();
     void setSteamId();
     void compare();
+    void dbusMarshall();
 
 };
 
@@ -54,6 +55,42 @@ void TestPlayerInfo::compare()
     b.setSteamId(SteamId(Q_UINT64_C(76561198074409147)));
 
     QCOMPARE(a, b);
+}
+
+void TestPlayerInfo::dbusMarshall()
+{
+    {
+        PlayerInfo a(123);
+        a.setName("fake name");
+        a.setSteamId(SteamId(Q_UINT64_C(76561198074409147)));
+
+        QDBusArgument dbusArgument;
+        dbusArgument << a;
+        QCOMPARE(dbusArgument.currentType(), QDBusArgument::ElementType::UnknownType);
+
+        PlayerInfo b;
+        dbusArgument >> b;
+        QCOMPARE(a, b);
+    }
+
+    {
+        PlayerInfo a(998);
+        a.setName("fake name 1");
+        a.setSteamId(SteamId(Q_UINT64_C(1)));
+
+        PlayerInfo b(999);
+        b.setName("fake name 2");
+        b.setSteamId(SteamId(Q_UINT64_C(2)));
+
+        PlayerInfoList listA({ a, b });
+        QDBusArgument dbusArgument;
+        dbusArgument << listA;
+        QCOMPARE(dbusArgument.currentType(), QDBusArgument::ElementType::UnknownType);
+
+        PlayerInfoList listB;
+        dbusArgument >> listB;
+        QCOMPARE(listA, listB);
+    }
 }
 
 QTEST_MAIN(TestPlayerInfo)
