8be2a93af7a6de7d00ce7a6aac6a7fd3fddad20f
Michał <michal@garapich.pl>
2019-11-04 14:55:26 +0100

    add TestServerStatus

--- test/testserverstatus.cpp
+++ test/testserverstatus.cpp
@@ -1,31 +1,77 @@
 #include <QtTest/QtTest>
+#include "servermanager.h"
 #include "serverstatus.h"
+#include "utils/testinggameservercontroller.h"
 
 using namespace morgoth;
 
 class TestServerStatus : public QObject {
     Q_OBJECT
+    ServerManager* serverManager;
+    Server* server;
+    ServerStatus* serverStatus;
 
 private slots:
-    void create();
-    
+    void initTestCase();
+    void init();
+    void cleanup();
+
+    void emptyAllProps();
+    void fetchProps();
 };
 
-void TestServerStatus::create()
+void TestServerStatus::initTestCase()
 {
-    Server* server = new Server(QUrl::fromLocalFile("/some/path"), "test");
-    ServerStatus status(server);
+    QDBusConnection con = QDBusConnection::sessionBus();
+    QVERIFY(con.isConnected());
+}
 
-    // verify all values are empty/zeroed
-    QCOMPARE(status.hostname(), QString());
-    QCOMPARE(status.playerCount(), 0);
-    QCOMPARE(status.maxPlayers(), 0);
-    QCOMPARE(status.map(), QString());
-    QCOMPARE(status.address(), QUrl());
-    QCOMPARE(status.password(), QString());
-    QCOMPARE(status.stvPort(), 0);
-    QCOMPARE(status.stvPassword(), QString());
-    QCOMPARE(status.players().size(), 0);
+void TestServerStatus::init()
+{
+    serverManager = new ServerManager();
+    QVERIFY(!serverManager->dbusServerAddress().isEmpty());
+
+    server = serverManager->add(QUrl::fromLocalFile("/some/path"), "test");
+    QVERIFY(server);
+
+    serverStatus = new ServerStatus(server);
+}
+
+void TestServerStatus::cleanup()
+{
+    delete serverStatus;
+    delete serverManager;
+}
+
+void TestServerStatus::emptyAllProps()
+{
+    // verify all properties are empty/zeroed
+    QCOMPARE(serverStatus->hostname(), QString());
+    QCOMPARE(serverStatus->playerCount(), 0);
+    QCOMPARE(serverStatus->maxPlayers(), 0);
+    QCOMPARE(serverStatus->map(), QString());
+    QCOMPARE(serverStatus->address(), QUrl());
+    QCOMPARE(serverStatus->password(), QString());
+    QCOMPARE(serverStatus->stvPort(), 0);
+    QCOMPARE(serverStatus->stvPassword(), QString());
+    QCOMPARE(serverStatus->players().size(), 0);
+}
+
+void TestServerStatus::fetchProps()
+{
+    TestingGameServerController controller;
+    controller.setPath("/some/path");
+    controller.start();
+    controller.setMaxPlayers(7);
+    controller.setAddress("127.0.0.1:27015");
+    controller.connect(serverManager->dbusServerAddress());
+
+    QTest::qWait(1000);
+
+    QCOMPARE(serverStatus->maxPlayers(), 7);
+    QCOMPARE(serverStatus->address(), QUrl("steam://127.0.0.1:27015"));
+
+    controller.stop();
 }
 
 QTEST_MAIN(TestServerStatus)
